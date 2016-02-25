18f10c0ae2cce9467e54b964df2bcbe52393b177
Mathias Schreiber <mathias.schreiber@wmdb.de>
2016-02-25 09:39:42 +0100

    Applied PSR2

--- ext_tables.php
+++ ext_tables.php
@@ -1,71 +1,71 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 if (!isset($_EXTKEY)) {
     $_EXTKEY = 'typo3_roadmap';
 }
 $TCA['tx_typo3roadmap_majorversion'] = array(
-	'ctrl' => array(
-		'title'     => 'LLL:EXT:typo3_roadmap/locallang_db.xml:tx_typo3roadmap_majorversion',		
-		'label'     => 'title',	
-		'tstamp'    => 'tstamp',
-		'crdate'    => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'sortby' => 'sorting',	
-		'delete' => 'deleted',
+    'ctrl' => array(
+        'title' => 'LLL:EXT:typo3_roadmap/locallang_db.xml:tx_typo3roadmap_majorversion',
+        'label' => 'title',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'sortby' => 'sorting',
+        'delete' => 'deleted',
         'dividers2tabs' => 1,
-		'enablecolumns' => array(		
-			'disabled' => 'hidden',	
-			'starttime' => 'starttime',	
-			'endtime' => 'endtime',
-		),
-		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
-		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_typo3roadmap_majorversion.gif',
-	),
+        'enablecolumns' => array(
+            'disabled' => 'hidden',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+        ),
+        'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
+        'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_typo3roadmap_majorversion.gif',
+    ),
 );
 
 $TCA['tx_typo3roadmap_minorversion'] = array(
-	'ctrl' => array(
-		'title'     => 'LLL:EXT:typo3_roadmap/locallang_db.xml:tx_typo3roadmap_minorversion',		
-		'label'     => 'version',
-		'tstamp'    => 'tstamp',
-		'crdate'    => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'sortby' => 'sorting',	
-		'delete' => 'deleted',	
-		'enablecolumns' => array(		
-			'disabled' => 'hidden',
-		),
-		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
-		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_typo3roadmap_minorversion.gif',
-	),
+    'ctrl' => array(
+        'title' => 'LLL:EXT:typo3_roadmap/locallang_db.xml:tx_typo3roadmap_minorversion',
+        'label' => 'version',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'sortby' => 'sorting',
+        'delete' => 'deleted',
+        'enablecolumns' => array(
+            'disabled' => 'hidden',
+        ),
+        'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
+        'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_typo3roadmap_minorversion.gif',
+    ),
 );
 
 $TCA['tx_typo3roadmap_phpversion'] = array(
-	'ctrl' => array(
-		'title'     => 'LLL:EXT:typo3_roadmap/locallang_db.xml:tx_typo3roadmap_phpversion',		
-		'label'     => 'version',
-		'tstamp'    => 'tstamp',
-		'crdate'    => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'sortby' => 'sorting',	
-		'delete' => 'deleted',	
-		'enablecolumns' => array(		
-			'disabled' => 'hidden',
-		),
-		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
-		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_typo3roadmap_phpversion.gif',
-	),
+    'ctrl' => array(
+        'title' => 'LLL:EXT:typo3_roadmap/locallang_db.xml:tx_typo3roadmap_phpversion',
+        'label' => 'version',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'sortby' => 'sorting',
+        'delete' => 'deleted',
+        'enablecolumns' => array(
+            'disabled' => 'hidden',
+        ),
+        'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
+        'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_typo3roadmap_phpversion.gif',
+    ),
 );
 
 
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages';
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages';
 
 
 t3lib_extMgm::addPlugin(array(
-	'LLL:EXT:typo3_roadmap/locallang_db.xml:tt_content.list_type_pi1',
-	$_EXTKEY . '_pi1',
-	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
-),'list_type');
+    'LLL:EXT:typo3_roadmap/locallang_db.xml:tt_content.list_type_pi1',
+    $_EXTKEY . '_pi1',
+    t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+), 'list_type');
